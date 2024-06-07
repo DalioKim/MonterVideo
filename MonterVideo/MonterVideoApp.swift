f0bb5478ef10c52c6db53e98bcd34c91b743bc29
kdh <ejxlabvlzhxm@naver.com>
2024-06-07 14:47:59 +0900

    Initial Commit

---
+++ MonterVideo/MonterVideoApp.swift
@@ -0,0 +1,20 @@
+//
+//  MonterVideoApp.swift
+//  MonterVideo
+//
+//  Created by 김동현 on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct MonterVideoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
